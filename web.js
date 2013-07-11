18e22a695371e406d3af662b9764b6d308c062ef
Victoria Liang <shirosaccount@gmail.com>
2013-07-11 06:23:15 +0000

    modified web.js

--- web.js
+++ web.js
@@ -1,11 +1,13 @@
 var express = require('express');
 
 var app = express.createServer(express.logger());
-var buf = new Buffer(200);
-var fs = require('fs');
-var text = buf.toString(fs.readFileSync('index.html')); 
 app.get('/', function(request, response) {
-  response.send(text);
+  
+  var buf = new Buffer();
+  var fs = require('fs');
+    var text = buf.toString('utf8', fs.readFileSync("index.html")); 
+    response.send(text);
+
 });
 
 var port = process.env.PORT || 5000;
