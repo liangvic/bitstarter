7b67d52dc45f71d50605b867e1dec744d21844fb
Victoria Liang <shirosaccount@gmail.com>
2013-07-11 06:35:56 +0000

    modified web.js

--- web.js
+++ web.js
@@ -3,7 +3,7 @@ var express = require('express');
 var app = express.createServer(express.logger());
 app.get('/', function(request, response) {
   
-  var buf = new buffer();
+  var buf = new Buffer();
   var fs = require('fs');
     response.send(buf.toString('utc 8', fs.readFileSync("index.html")));
 
