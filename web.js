a8e17408ce96bb42607c99831896ddf5b1fb1d27
Victoria Liang <shirosaccount@gmail.com>
2013-07-11 06:07:02 +0000

    modified web.js

--- web.js
+++ web.js
@@ -1,9 +1,11 @@
 var express = require('express');
 
 var app = express.createServer(express.logger());
-
+var buf = new Buffer(200);
+var fs = require('fs');
+var text = buf.toString(fs.readFileSync('index.html')); 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send(text);
 });
 
 var port = process.env.PORT || 5000;
